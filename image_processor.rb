09ef01b26c5f6b4b4bd27f76b152fc272bf8092d
Sammy Larbi <sam@codeodor.com>
2011-08-21 21:10:32 -0500

    added sample sheet music, started image processor which uses rmagick to count rows and columns

---
+++ image_processor.rb
@@ -0,0 +1,40 @@
+require 'RMagick'
+
+class ImageProcessor
+  include Magick
+  attr_accessor :image
+  
+  def initialize(path_to_image)
+    @image = ImageList.new(path_to_image)
+  end
+  
+  def display
+    @image.display
+  end
+  
+  def rows
+    result = []
+    (0..(@image.rows-1)).each do |row_num|
+      result << @image.get_pixels(0, row_num, @image.columns, 1)
+    end
+    return result
+  end
+  
+  def columns
+    result = []
+    (0..(@image.columns-1)).each do |col_num|
+      result << @image.get_pixels(col_num, 0, 1, @image.rows)
+    end
+    return result
+  end 
+  
+end
+
+
+ip = ImageProcessor.new('star_wars_main_title.jpg')
+
+puts ip.image.rows
+puts ip.rows.count
+puts ip.image.columns
+puts ip.columns.count
+
