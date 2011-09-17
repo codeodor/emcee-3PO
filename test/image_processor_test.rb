143fb73a60351aa0b0d0aec62a7acfe822215ffc
Sammy Larbi <sam@codeodor.com>
2011-09-17 13:49:20 -0500

    fix tests for x and y projection by adding known image to test

--- test/image_processor_test.rb
+++ test/image_processor_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/image_processor'
 
 class ImageProcessorTest < Test::Unit::TestCase
   def setup
-    @ip = ImageProcessor.new('star_wars_main_title.jpg')
+    @ip = ImageProcessor.new('test/projection_test.gif')
   end
   
   def test_number_of_rows_found_is_number_of_rows_in_image
@@ -17,16 +17,12 @@ class ImageProcessorTest < Test::Unit::TestCase
   
   def test_y_projection_gives_accurate_count_of_dark_pixels_per_row
     assert @ip.y_projection.count == @ip.image.rows, "The y projection was not equal to the number of rows"
-    @ip.y_projection.each_with_index do |count_in_row, i|
-      assert_equal 0, count_in_row
-    end
+    assert_equal @ip.y_projection, [11, 6, 7, 1, 1, 12, 1, 1, 1, 2, 20, 2, 11, 2, 1, 8, 10, 2, 17, 0]
   end
   
   def test_x_projection_gives_accurate_count_of_dark_pixels_per_column
     assert @ip.x_projection.count == @ip.image.columns, "The x projection was not equal to the number of columns"
-    @ip.x_projection.each_with_index do |count_in_row, i|
-      assert_equal 0, count_in_row
-    end
+    assert_equal @ip.x_projection, [6, 5, 6, 7, 6, 5, 5, 5, 6, 6, 5, 5, 1, 19, 5, 4, 3, 6, 5, 6]    
   end
   
   
