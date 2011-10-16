72e18bbe6fe7284e350c5f323112cc32435a8650
Sammy Larbi <sam@codeodor.com>
2011-10-16 12:08:38 -0500

    improve range expansion to try to include notes off the staff lines

--- lib/optical_music_recognizer.rb
+++ lib/optical_music_recognizer.rb
@@ -6,13 +6,16 @@ class OpticalMusicRecognizer
   end
   
   def staves
-    threshold = @image_processor.y_projection.inject(:+) / @image_processor.y_projection.count
-    local_maxima = @image_processor.y_projection.local_maxima(threshold)
+    local_maxima = @image_processor.y_projection.local_maxima(threshold_for_stave_detection)
     return stave_ranges(local_maxima)
   end
   
   
   private
+  def threshold_for_stave_detection
+    @image_processor.y_projection.inject(:+) / @image_processor.y_projection.count
+  end
+  
   def tightest_cluster(windows)
     distances = windows.map{|x| x[-1] - x[0]}
     return windows[distances.index(distances.min)]
@@ -35,7 +38,13 @@ class OpticalMusicRecognizer
   end
   
   def expand_to_include_off_staff_notes(staff_ranges)
-    return staff_ranges
+    return staff_ranges.map do |range|
+      lower_index = staff_ranges.index(range) > 0 ? staff_ranges.fetch(staff_ranges.index(range)-1).first : 0
+      upper_index = staff_ranges.fetch(staff_ranges.index(range)+1, range.last..@image_processor.rows.count-1).last
+      new_first = @image_processor.y_projection.to_a[lower_index..range.first].rindex{|x| x < threshold_for_stave_detection / 4}.to_i + lower_index
+      new_last = @image_processor.y_projection.to_a[range.last..upper_index].index{|x| x < threshold_for_stave_detection / 4} + range.last
+      new_first..new_last
+    end
   end
 
 end
